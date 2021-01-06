2c42b7bd86b6ce047678abc80a64afe73f2cbc3d
Oğuzhan Türker <oguzturker8@gmail.com>
2021-01-06 12:49:21 +0300

    vercel osp done

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,53 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Rollie</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <h1 className={styles.title}>Rollie</h1>
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Roll a movie to your watcher oh valley of plenty!
         </p>
-
+        <div className={styles.imgWrapper}>
+          {[1, 2, 3].map((item) => (
+            <img
+              src={"/img" + item + ".png"}
+              alt={"Rollie" + item + " Photo"}
+            />
+          ))}
+        </div>
+        <p className={styles.description}>
+          Find your next favorite among over 1000 iMDB 7+ movies.
+        </p>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a
+            href="https://github.com/oguzturker8/Rollie"
+            className={styles.card}
+          >
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about Rollie on Github.</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="#" className={styles.card}>
+            <h3>Download &rarr;</h3>
+            <p>Download Rollie on your device.{`\n`} </p>
           </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
---
+++ styles/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  black: "#0c0f0a",
+  dark: "#02040f",
+  purple: "#665DF5",
+  pink: "#ff206e",
+  yellow: "#F3E600",
+  blue: "#4cc9f0",
+  red: "#d00000",
+  white: "#f8edeb",
+  gray: "#8F8D8D",
+  green: "#70e000",
+};
+
+export default colors;
